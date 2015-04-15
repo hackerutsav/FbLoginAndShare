6356dfbafe4d725d63d53dc608156dcef5c4c51f
Utsav Kumar <hackerutsav@yahoo.com>
2015-04-15 22:08:46 +0530

    Toast to show whether logged in or not.

--- app/src/main/java/authorwjf/example/testingfb/MainActivity.java
+++ app/src/main/java/authorwjf/example/testingfb/MainActivity.java
@@ -4,15 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -25,27 +28,29 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-        LoginButton button  = (LoginButton) findViewById(R.id.fblogin_button);
+        setContentView(R.layout.activity_main);
+        LoginButton button  = (LoginButton) findViewById(R.id.fb_login_button);
         button.setReadPermissions(Arrays.asList("user_friends"));
-        final Activity a = this;
+        final Activity z = this;
 
         button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                @Override
-                public void onSuccess (LoginResult loginResult)
+                Log.i("fblog" , AccessToken.getCurrentAccessToken().getToken());
+                Toast.makeText(z, Profile.getCurrentProfile().getName(),Toast.LENGTH_LONG).show();
+
             }
 
             @Override
             public void onCancel() {
-
+                Toast.makeText(z,"Cancelled",Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onError(FacebookException e) {
+                Toast.makeText(z,e.toString(),Toast.LENGTH_LONG).show();
 
             }
         });
@@ -53,7 +58,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onActivityResult(int requestCode ,int resultCode , Intent data ) {
-        super.onActivityResult(int requestCode , resultCode , data);
+        super.onActivityResult( requestCode , resultCode , data);
         callbackManager.onActivityResult(requestCode , resultCode , data );
     }
 
